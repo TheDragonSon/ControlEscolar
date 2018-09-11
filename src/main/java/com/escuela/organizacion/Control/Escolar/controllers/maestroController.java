702010d8918967731a52287cb4aad11a266b17bf
TheDragonSon <greym_9@hotmail.com>
2018-09-11 10:27:46 -0500

    agregue los objetos a la página regalumno y regstrovalido y maestroaux

--- src/main/java/com/escuela/organizacion/Control/Escolar/controllers/maestroController.java
+++ src/main/java/com/escuela/organizacion/Control/Escolar/controllers/maestroController.java
@@ -1,6 +1,10 @@
 package com.escuela.organizacion.Control.Escolar.controllers;
 
+import com.escuela.organizacion.Control.Escolar.dao.ImaestroDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 public class maestroController {
 
