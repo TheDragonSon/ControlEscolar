1536bdeea0ada8e1112eaff793c7aede020172d6
WinterDarknesS <miguel_8871@hotmail.com>
2018-09-11 09:58:03 -0500

    creacion de pagina para creacion de alumnos

--- src/main/java/com/escuela/organizacion/Control/Escolar/ControlEscolarApplication.java
+++ src/main/java/com/escuela/organizacion/Control/Escolar/ControlEscolarApplication.java
@@ -2,9 +2,10 @@ package com.escuela.organizacion.Control.Escolar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ControlEscolarApplication {
+public class ControlEscolarApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControlEscolarApplication.class, args);
